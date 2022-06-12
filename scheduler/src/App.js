9a3c2a1fd30d8320cff7431b165ebb6b427b8464
xamaxmesiter <xanaxmeister@idonthave.com>
2022-06-12 20:34:01 +0200

    Somethin there#

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
 import React from 'react';
 
 const schedule = {
-title: "CS Courses for 2018-2019" };
+  title: "Northside Youth Soccer League",
+  text: ""
+};
 
 const App = () =>  (
   <div>
     <h1>{ schedule.title }</h1>
-  </div>
+    <br></br>
+    <h2>{"Upcoming Events"}</h2>
+    <br></br>
+    <div>Home About NYSL Contact Rules and Policies Game Information Registration Form</div>
+    <br></br>
+    <div>August 4</div>
+    <br></br>
+    <div>NYSL Fundraiser</div>
+    <br></br>
+    <div>August 16</div>
+    <br></br>
+    <div>Season Kick-off: Meet the Teams</div>
+    <br></br>
+    <div>September 1</div>
+    <br></br>
+    <div>First Game of the Season (Check Game Schedule for details)</div>
+</div>
 );
 
 export default App;
