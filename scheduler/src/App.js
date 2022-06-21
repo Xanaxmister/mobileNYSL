3486c838b2a10d835d83eb3e757f9c3434d5f9e8
xamaxmesiter <xanaxmeister@idonthave.com>
2022-06-21 19:54:28 +0200

    trask2 done

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,105 +1,18 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-
-const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
-
-
-const Banner = ({ title }) => (
-  <h1>{title}</h1>
-);
-
-const CourseList = ({ courses }) => {
-  const [term, setTerm] = useState('Fall');
-  const [selected, setSelected] = useState([]);
-  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
-
-  return (
-    <>
-      <TermSelector term={term} setTerm={setTerm} />
-      <div className="course-list">
-        {
-          termCourses.map(course =>
-            <Course key={course.id} course={course}
-              selected={selected} setSelected={setSelected}
-            />)
-        }
-      </div>
-    </>
-  );
-};
-
-const toggle = (x, lst) => (
-  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
-);
-
-const TermButton = ({ term, setTerm, checked }) => (
-  <>
-    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
-      onChange={() => setTerm(term)} />
-    <label className="btn btn-success m-1 p-2" htmlFor={term}>
-      {term}
-    </label>
-  </>
-);
-
-const TermSelector = ({ term, setTerm }) => (
-  <div className="btn-group">
-    {
-      Object.values(terms).map(value => (
-        <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
-      ))
-    }
-  </div>
-);
-
-const Course = ({ course, selected, setSelected }) => {
-  const isSelected = selected.includes(course);
-  const style = {
-    backgroundColor: isSelected ? 'lightgreen' : 'white'
-  };
-  return (
-    <div className="card m-1 p-2"
-      style={style}
-      onClick={() => setSelected(toggle(course, selected))}>
-      <div className="card-body">
-        <div className="card-title">{getCourseTerm(course)} CS {getCourseNumber(course)}</div>
-        <div className="card-text">{course.title}</div>
-      </div>
-    </div>
-  );
-};
-
-
-const getCourseTerm = course => (
-  terms[course.id.charAt(0)]
-);
-
-const getCourseNumber = course => (
-  course.id.slice(1, 4)
-);
-
-//EXPLAIN ! ! ! ! !
+import { addScheduleTimes, Banner } from './utilities/times.js';
+import CourseList from './components/CourseList';
+import { useData } from './utilities/firebase.js';
 
 const App = () => {
-  const [schedule, setSchedule] = useState();
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
-
-  useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
-    }
-    fetchSchedule();
-  }, []);                                              // [URL] ?!?!?!
-
-  if (!schedule) return <h1>Loading schedule...</h1>;
+  const [schedule, loading, error] = useData('/', addScheduleTimes); 
+  
+  if (error) return <h1>{error}</h1>;
+  if (loading) return <h1>Loading the schedule...</h1>
 
   return (
     <div className="container">
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
     </div>
   );
 };
